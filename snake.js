5a06b60a9871697ff97699607730f58349f3942c
Sai Bulusu <sai.bulusu@gmail.com>
2018-05-28 14:02:40 -0700

    Fixed bug: generate a new apple

--- snake.js
+++ snake.js
@@ -1,28 +1,35 @@
+// define the canvas
 var canvas;
 var canvasContext;
 
+// parameters of the snake
 var xPos = 500;
 var yPos = 300;
 var xSpeed = 0;
 var ySpeed = 0;
-
 var edge = 20;
 
+// parameters of the apple
 var appleX = 100;
 var appleY = 100;
 
+// holds each new position of the snake after eating the apple
 var positions = [];
 positions[0] = [xPos, yPos];
 
+// helps add a new value to the end of the snake after eating an apple
 var newX = -1;
 var newY = -1;
 
+// the direction of the snake to ensure that illegal turns are avoided
 var direction = "pause";
 
-window.onload = function() {
+window.onload = function() { // when the page loads
+	// define the canvas to the webpage
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
 
+	// key events are handled
 	window.addEventListener('keydown', keyInput, false);
 
 	// run 30 frames per second
@@ -30,115 +37,130 @@ window.onload = function() {
 	setInterval(updateAll, 1000/framesPerSecond);
 }
 
-function keyInput(event) {
+function keyInput(event) { // key events to control the direction of the snake
 	// alert(event.keyCode);
+
+	// code holds the number to map to a direction, using arrow-keys
 	var code = event.keyCode;
 
-	if (code == 37) {
+	if (code == 37) { // left
 		if (direction != "right") {
 			xSpeed = -20;
 			ySpeed = 0;
 			direction = "left";
 		}
-	} else if (code == 39) {
+	} else if (code == 39) { // right
 		if (direction != "left") {
 			xSpeed = 20;
 			ySpeed = 0;
 			direction = "left";
 		}
-	} else if (code == 38) {
+	} else if (code == 38) { // up
 		if (direction != "down") {
 			ySpeed = -20;
 			xSpeed = 0;
 			direction = "up";
 		}
-	} else if (code == 40) {
+	} else if (code == 40) { // down
 		if (direction != "up") {
 			ySpeed = 20;
 			xSpeed = 0;
 			direction = "down";
 		}
-	} else if (code == 32) {
-		xSpeed = 0;
-		ySpeed = 0;
-		direction = "pause";
-		console.log(appleX + " " + appleY + " " + xPos + " " + yPos);
+	} else if (code == 32) { // space to pause (debugging)
+		// xSpeed = 0;
+		// ySpeed = 0;
+		// direction = "pause";
+		// console.log(appleX + " " + appleY + " " + xPos + " " + yPos);
 	}
 }
 
-function updateAll() {
+function updateAll() { // call this with every frame
+	// move and draw everything over the current canvas
 	moveAll();
 	drawAll();
 }
 
-function moveAll() {
-	if (!isLegal()) {
+function moveAll() { // update the speed of the snake
+	if (!isLegal()) { // if the current location of the snake is illegal, then reset the snake
 		reset();
 	}
 
-	if (xPos == appleX && yPos == appleY) {
+	if (xPos == appleX && yPos == appleY) { // if the snake's head has gone over an apple
+		// set the new values to this location to create a new head later
 		newX = appleX;
 		newY = appleY;
+		// create a new apple somewhere
 		updateApple();
 	}
 
-	for (i = positions.length - 1; i >= 1; i--) {
+	for (i = positions.length - 1; i >= 1; i--) { // for every position in the snake, update to the next position
 		positions[i][0] = positions[i - 1][0];
 		positions[i][1] = positions[i - 1][1];
 	}
 
+	// the head of the snake keeps going in the direction of the speed
 	positions[i][0] += xSpeed;
 	positions[i][1] += ySpeed;
 
-	if (newX != -1) {
+	if (newX != -1) { // if the apple was consumed
+		// push in the new position
 		positions.push([newX, newY]);
 		newX = -1;
 		newY = -1;
 	}
 
+	// xPos and yPos will be easier to call later
 	xPos = positions[0][0];
 	yPos = positions[0][1];
 }
 
-function reset() {
-	// xPos = 500;
-	// yPos = 300;
+function reset() { // reset the location of the snake
+	// set the length of the snake back to 1
 	positions = new Array();
 	positions[0] = [500, 300];
 
+	// reset the speed in both directions
 	xSpeed = 0;
 	ySpeed = 0;
 
+	// have a defined location for the apple
 	appleX = 100;
 	appleY = 100;
 
+	// it will not be moving
 	direction = "pause";
 }
 
-function updateApple() {
+function updateApple() { // create a new location for an apple
+	// the apple has to be within the boundaries on the sides of the canvas
 	var lowerBoundX = 1;
 	var upperBoundX = (canvas.width - edge) / edge;
 
 	var lowerBoundY = 1;
 	var upperBoundY = (canvas.height - edge - 4) / edge;
 
+	// randomly set a location within those given bounds
 	appleX = Math.floor(Math.random() * (upperBoundX - lowerBoundX) + lowerBoundX) * edge;
 	appleY = Math.floor(Math.random() * (upperBoundY - lowerBoundY) + lowerBoundY) * edge;
+
+	// make sure that the new apple is not sharing a location with the snake
+	for (i = 0; i < positions.length; i++) {
+		if (appleX == positions[i][0] && appleY == positions[i][1]) {
+			// recursively call updateApple()
+			updateApple();
+		}
+	}
 }
 
-function drawAll() {
+function drawAll() { // update the location of everything in the canvas
 	// white background for the canvas
 	colorRect(0, 0, canvas.width, canvas.height, 'black');
 
 	// snake in the canvas
-	// colorRect(xPos, yPos, edge, edge, 'white');
-	// colorRect(xPos + edge, yPos, edge, edge, 'white');\
 	for (i = 0; i < positions.length; i++) {
 		colorSquare(positions[i][0], positions[i][1], edge, 'black', 'white');
 	}
-
-	// colorSquare(xPos, yPos, edge, 'black', 'white');
-	// colorSquare(xPos + edge, yPos, edge, 'black', 'white');
 
 	// borders
 	colorRect(0, 0, canvas.width, edge, 'white');
@@ -149,10 +171,12 @@ function drawAll() {
 	// apple
 	colorSquare(appleX, appleY, edge, 'grey', 'grey');
 
+	// score
 	colorText(positions.length + "", canvas.width / 2, canvas.height - 10, 'black');
 }
 
-function isLegal() {
+function isLegal() { // check if the given location of the snake is legal
+	// check if the snake head is within the borders
 	if (xPos < edge) {
 		return false;
 	}
@@ -170,14 +194,17 @@ function isLegal() {
 	}
 
 	for (i = 1; i < positions.length; i++) {
+		// if the positions besides the head share a location with the head, then this is not legal (self-collision)
 		if (positions[0][0] == positions[i][0] && positions[0][1] == positions[i][1]) {
 			return false;
 		}
 	}
 
+	// if nothing fails to be legal, then this is a legal position
 	return true;
 }
 
+// helper function to draw a square
 function colorSquare(topLeftX, topLeftY, edge, color1, color2) {
 	canvasContext.fillStyle = color1;
 	canvasContext.fillRect(topLeftX, topLeftY, edge, edge, color1);
